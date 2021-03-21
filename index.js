75fe430e9e2ee4c30e200150dc409fc70c82ee13
Konstantin Tieber <hello@xkons.de>
2021-03-21 14:14:59 +0100

    Optimiere die Skalierung des Spiels auf unterschiedlichen Bildschimgrößen
    Diese Lösung habe ich mir von einem anderen Phaser Projekt
    aus einem Berufspraktikum kopiert:
    https://github.com/SidneyK1/pong

--- index.js
+++ index.js
@@ -121,6 +121,15 @@ class Noodlejump extends NoodlejumpAdvanced {
  * Dieser Javascript-Code wird als letztes von der Webseite geladen und 
  * startet das Spiel, dass wir in der Klasse "Noodlejump" erstellt haben.
  */
-const game = new Phaser.Game(300, 500, Phaser.CANVAS, '');
+const game = new Phaser.Game({
+  height: 500,
+  width: 300,
+  renderer: Phaser.CANVAS,
+  scaleMode: Phaser.ScaleManager.SHOW_ALL, // Fenstergröße (https://photonstorm.github.io/phaser-ce/Phaser.ScaleManager.html)
+  parent: 'container',
+  alignH: true,
+  alignV: true
+});
+
 game.state.add('Noodlejump', Noodlejump);
 game.state.start('Noodlejump');
